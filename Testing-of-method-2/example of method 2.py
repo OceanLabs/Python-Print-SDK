7fb21b9bb27f5601c8ba928b7a690b00cb4a4a97
victor-baumbach <baumbach.victor@gmail.com>
2014-07-16 16:57:24 +0000

    Update and rename example of method 2 to example of method 2.py

---
+++ Testing-of-method-2/example of method 2.py
@@ -0,0 +1,37 @@
+edited_parameters = {
+'setting_a' : '',
+'setting_b' : '',
+'setting_c' : '',
+'setting_d' : ''
+}
+
+default_parameters = default_example_parameters()
+
+def example_ordering_module(edited_parameters):
+  from method_2_test_defaults import default_example_parameters
+  default_parameters = default_example_parameters()
+  
+  Ledited_parameters = [
+  edited_parameters['setting_a'],
+  edited_parameters['setting_b'],
+  edited_parameters['setting_c'],
+  edited_parameters['setting_d']
+  ]
+  
+  Ldefault_parameters = [
+  default_parameters['setting_a'],
+  default_parameters['setting_b'],
+  default_parameters['setting_c'],
+  default_parameters['setting_d'],
+  ]
+  
+  for i in len(Ledited_parameters):
+    if Ledited_parameter[i] != '':
+      Ldefault_parameter[i] = Ledited_parameter[i]
+  
+  default_parameters['setting_a'] = Ldefault_parameter[0]
+  default_parameters['setting_b'] = Ldefault_parameter[1]
+  default_parameters['setting_c'] = Ldefault_parameter[2]
+  default_parameters['setting_d'] = Ldefault_parameter[3]
+  
+  place_order_parameters(default_parameters)
