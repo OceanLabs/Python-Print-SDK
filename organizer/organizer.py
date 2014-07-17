9fa46f484a1040ab5b97afe2659d50ff1e6263b1
victor-baumbach <baumbach.victor@gmail.com>
2014-07-17 12:21:55 +0100

    Back up
    Backed up organizer work done on IDLE to github.

---
+++ organizer/organizer.py
@@ -0,0 +1,35 @@
+import requests
+
+class template(object):
+
+    def __init__(self, other)
+
+        blob = json.fetch(URL, passcode)
+        cost = blob[5]
+        #ect.
+
+    def ship_it(self, identifier, yada):
+        
+        self.blob[identifier] = yada
+                
+        json.put(URL, self.blob[identifier], passcode)
+
+
+
+
+
+##    def _headers(self):
+##        return {
+##            "Authorization": "ApiKey {}:{}".format(self.public_key, self.secret_key),
+##            "Content-Type": "application/json",
+##        }
+##
+##    def _get_request(self, url, params=None):
+##        url = os.path.join(self.base_url, url)
+##        r = requests.get(url, headers=self._headers)
+##        return r.json()
+##
+##    def _post_request(self, url, data):
+##        url = os.path.join(self.base_url, url)
+##        r = requests.post(url, data=data, headers=self._headers)
+##        return r.json()
