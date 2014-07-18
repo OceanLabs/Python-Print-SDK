f301107a391627efbe7b1ea54d29e0cb9f030116
Victor <baumbach.victor@gmail.com>
2014-07-18 17:27:40 +0100

    Update searcher.py

--- organizer/searcher.py
+++ organizer/searcher.py
@@ -62,18 +62,94 @@ class searcher:
                 self.bottom_grip = m[u'bottom_grip']
                 self.colors = m[u'colors']
                 self.group_gutter = m[u'group_gutter']
-                self.groups = [m[u'groups_x'], m[u'groups_y']]
+                self.groups_x = m[u'groups_x']
+                self.groups_y = m[u'groups_y']
                 self.gutter_bleed = m[u'gutter_bleed']
                 self.image_replacements = m[u'image_replacements']
                 self.is_image_grid = m[u'is_image_grid']
                 self.left_grip = m[u'left_grip']
-                self.n = [m[u'nx'], m[u'ny']]
+                self.nx = m[u'nx']
+                self.ny = m[u'ny']
                 self.pages = m[u'pages']
                 self.paragraph_styles = m[u'paragraph_styles']
                 self.polaroid_grip = m[u'polarpoid_grip']
-                self.unit = [m[u'unit_height'], m[u'unit_width']]
+                self.unit_height = m[u'unit_height']
+                self.unit_width = m[u'unit_width']
             else:
-                break
+                if u'address_code_index' in o.keys():
+                    self.address_code_index = o[u'address_code_index']
+                else:
+                    self.address_code_index = m[u'address_code_index']
+                if u'border' in o.keys():
+                    self.border = o[u'border']
+                else:
+                    self.border = m[u'border']
+                if u'bottom_grip' in o.keys():
+                    self.bottom_grip = o[u'bottom_grip']
+                else:
+                    self.bottom_grip = m[u'bottom_grip']
+                if u'colors' in o.keys():
+                    self.colors = o[u'colors']
+                else:
+                    self.colors = m[u'colors']
+                if u'group_gutter' in o.keys():
+                    self.group_gutter = o[u'group_gutter']
+                else:
+                    self.group_gutter = m[u'group_gutter']
+                if u'groups_x' in o.keys():
+                    self.groups_x = o[u'groups_x']
+                else:
+                    self.groups_x = m[u'groups_x']
+                if u'groups_y' in o.keys():
+                    self.groups_y = o[u'groups_y']
+                else:
+                    self.groups_y = m[u'groups_y']
+                if u'gutter_bleed' in o.keys():
+                    self.gutter_bleed = o[u'gutter_bleed']
+                else:
+                    self.gutter_bleed = m[u'gutter_bleed']
+                if u'image_replacements' in o.keys():
+                    self.image_replacements = o[u'image_replacements']
+                else:
+                    self.image_replacements = m[u'image_replacements']
+                if u'is_image_grid' in o.keys():
+                    self.is_image_grid = o[u'is_image_grid']
+                else:
+                    self.is_image_grid = m[u'is_image_grid']
+                if u'left_grip' in o.keys():
+                    self.left_grip = o[u'left_grip']
+                else:
+                    self.left_grip = m[u'left_grip']
+                if u'nx' in o.keys():
+                    self.nx = o[u'nx']
+                else:
+                    self.nx = m[u'nx']
+                if u'ny' in o.keys():
+                    self.ny = o[u'ny']
+                else:
+                    self.ny = m[u'ny']
+                if u'pages' in o.keys():
+                    self.pages = o[u'pages']
+                else:
+                    self.pages = m[u'pages']
+                if u'paragraph_styles' in o.keys():
+                    self.paragraph_styles = o[u'paragraph_styles']
+                else:
+                    self.paragraph_styles = m[u'paragraph_styles']
+                if u'polarpoid_grip' in o.keys():
+                    self.polarpoid_grip = o[u'polarpoid_grip']
+                else:
+                    self.polarpoid_grip = m[u'polarpoid_grip']
+                if u'unit_height' in o.keys():
+                    self.unit_height = o[u'unit_height']
+                else:
+                    self.unit_height = m[u'unit_height']
+                if u'unit_width' in o.keys():
+                    self.unit_width = o[u'unit_width']
+                else:
+                    self.unit_width = m[u'unit_width']
+                    
+                    
         elif product_id == u'polaroids':
             break
         elif product_id == u'default_postcard':
