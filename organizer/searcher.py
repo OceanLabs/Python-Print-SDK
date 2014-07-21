5a406855c32689df4d53eb1710404aa7a9c025e0
Victor <baumbach.victor@gmail.com>
2014-07-21 12:46:56 +0100

    Update searcher.py

--- organizer/searcher.py
+++ organizer/searcher.py
@@ -42,6 +42,18 @@ class searcher:
             return nothing
         else:
             return m[u'content_overrides']
+            
+    def edit_thing(thing_name, thing_data, product_id):
+        product = self.id_search(product_id)
+        if product[u'content_overrides'] == "null":
+            product[u'content_overrides'] = {}
+            product[u'content_overrides'][thing_name] = thing_data
+        else:
+            product[u'content_overrides'][thing_name] = thing_data
+        for i in range(0, len(self.blob[u'objects'])):
+            if self.blob[u'objects'][i][u'template_id'] == product_id:
+                self.blob[u'objects'][i] = product
+        post(self.blob)
 
     def burp():
         return self.glob
