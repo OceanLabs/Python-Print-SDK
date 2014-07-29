f12c7050cb0cf67a9c74e97d347c3761e729e74f
Victor Baumbach <baumbach.victor@gmail.com>
2014-07-29 13:19:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     url='https://github.com/OceanLabs/Python-Print-SDK',
     download_url = 'https://github.com/OceanLabs/Python-Print-SDK/tarball/0.1.0',
     license='LICENSE.txt',
-    description='Useful stuff.',
+    description='This is the official Kite python module from OceanLabs. You will require a public and private key from https://www.kite.ly/ in order to run',
     long_description=open('README.txt').read(),
     install_requires=[
         "requests == 2.3.0",
