391955ae5117a42a19c10ef1f51ca6727376b837
Victor Baumbach <baumbach.victor@gmail.com>
2014-07-30 16:49:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from distutils.core import setup
 setup(
     name='Python-Print-SDK',
     version='0.1.0',
-    author='V. Baumbach',
+    author='Victor Baumbach',
     author_email='baumbach.victor@gmail.com',
     packages=['kite'],
     scripts=[],
-    url='',
+    url='https://github.com/OceanLabs/Python-Print-SDK',
+    download_url = 'https://github.com/OceanLabs/Python-Print-SDK/tarball/0.1.0',
     license='LICENSE.txt',
-    description='Useful stuff.',
+    description='An API that allows printing polaroids, pictures, and postcards.',
+    keywords = ['printing', 'print', 'print API', 'print SDK', 'SDK', 'software development kit'],
     long_description=open('README.txt').read(),
     install_requires=[
         "requests == 2.3.0",
